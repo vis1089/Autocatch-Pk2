c0b7af947b644cd04c4625542c04da5e955ed829
Kameil <130675534+Kameil@users.noreply.github.com>
2023-12-16 10:33:49 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -145,9 +145,8 @@ async def on_message(message):
                     timesleep = random.uniform(1.5, 7.5)
                     await asyncio.sleep(0)
                     if not paused:
-                        async with message.channel.typing():
-                            await asyncio.sleep(timesleep)
-                            await message.channel.send('<@716390085896962058> h')
+                        await asyncio.sleep(timesleep)
+                        await message.channel.send('<@716390085896962058> h')
             else:
                 content = message.content
                 if 'The pokémon is ' in content:
